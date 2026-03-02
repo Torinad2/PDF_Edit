998311452df99a6c2fc2ee04697179c76c6b905a
Torinad2 <157666959+Torinad2@users.noreply.github.com>
2026-03-02 15:41:50 -0500

    1

---
+++ main.py
@@ -0,0 +1,28 @@
+#pip install pymupdf
+
+import fitz #PyMuPDF
+
+SOURCE_FILE = r"G:/C++.pdf"
+PAGES_PER_FILE = 50
+
+def main():
+    split_pdf()
+
+def split_pdf():
+
+    source_file = fitz.open(SOURCE_FILE)
+
+    for index in range(0, len(source_file), PAGES_PER_FILE):
+        output_file = fitz.open()
+
+        for j_index in range(index, min (index + PAGES_PER_FILE, len(source_file))):
+            output_file.insert_pdf(source_file, from_page= j_index, to_page= j_index)
+
+
+        output_file.save(f"part_{index//PAGES_PER_FILE + 1}.pdf")
+        output_file.close()
+
+    source_file.close()
+
+if __name__ == "__main__":
+    main()
