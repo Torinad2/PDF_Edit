d9babde1adda48f953fab6e2b7d1e5ae0d7a3cff
Torinad2 <157666959+Torinad2@users.noreply.github.com>
2026-03-02 20:24:08 -0500

    3

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 #pip install pymupdf
 
-import fitz #PyMuPDF
+#import fitz #PyMuPDF
+from pprint import pprint
+import pymupdf
 
 SOURCE_FILE = r"G:/C++.pdf"
-PAGES_PER_FILE = 50
+PAGES_PER_FILE = 10
 
 def main():
-    split_pdf()
+    split_pdf(SOURCE_FILE, PAGES_PER_FILE)
 
-def split_pdf():
+def split_pdf(input_file, pages_per_file):
 
-    source_file = fitz.open(SOURCE_FILE)
+    source_file = pymupdf.open(input_file) # or pymupdf.Document(filename)
 
-    for index in range(0, len(source_file), PAGES_PER_FILE):
-        output_file = fitz.open()
+    for i in range(0, len(source_file), pages_per_file):
+        output_file = pymupdf.open()
 
-        for j_index in range(index, min (index + PAGES_PER_FILE, len(source_file))):
-            output_file.insert_pdf(source_file, from_page= j_index, to_page= j_index)
+        for j in range(i, min (i + pages_per_file, len(source_file))):
+            output_file.insert_pdf(source_file, from_page= j, to_page= j)
 
-
-        output_file.save(f"G:/C++/part_{index//PAGES_PER_FILE + 1}.pdf")
+        part = (i // pages_per_file) + 1
+        output_file.save(f"G:/C++/part_{part}.pdf")
         output_file.close()
+
+        print(f"Chapter {part} created")
 
     source_file.close()
 
